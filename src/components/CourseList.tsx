2fe31377ffa233e6615503d93f46eba884e6903e
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2026-04-24 15:58:57 -0500

    my selection version

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
 import type { Term } from './TermSelector';
+
+
+const toggleList = <T,>(x: T, lst: T[]): T[] => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [...lst, x]
+);
+
 
 interface Course {
   term: string;
@@ -12,21 +19,30 @@ export interface CourseListProps {
   selectedTerm: Term;
 }
 
-const CourseList = ({ courses, selectedTerm }: CourseListProps) => (
-  <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 p-4">
-    {Object.entries(courses).filter(([, course]) => course.term === selectedTerm).map(([id, course]) => (
-      <div key={id} className="flex flex-col justify-between border rounded-lg p-4">
-        <div>
-          <h2 className="font-bold text-lg mb-2">{course.term} CS {course.number}</h2>
-          <p className="text-gray-600">{course.title}</p>
+const CourseList = ({ courses, selectedTerm }: CourseListProps) => {
+
+  const [selectedCourses, setSelectedCourses] = useState<Course[]>([]);
+
+  const toggleCourses = (item: Course) => {
+    setSelectedCourses(selectedCourses => toggleList(item, selectedCourses));
+  };
+  
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 p-4">
+      {Object.entries(courses).filter(([, course]) => course.term === selectedTerm).map(([id, course]) => (
+        <div key={id} className={`course-card${selectedCourses.includes(course) ? ' selected' : ''}`} onClick={() => toggleCourses(course)}>
+          <div>
+            <h2 className="font-bold text-lg mb-2">{course.term} CS {course.number}</h2>
+            <p className="text-gray-600">{course.title}</p>
+          </div>
+          <div>
+            <hr className="my-3 border-gray-200" />
+            <p className="text-gray-500 text-sm">{course.meets}</p>
+          </div>
         </div>
-        <div>
-          <hr className="my-3 border-gray-200" />
-          <p className="text-gray-500 text-sm">{course.meets}</p>
-        </div>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+  )
+};
 
 export default CourseList;
