ab4682a7df0322a658f1a534fcc9f800f9746126
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2026-06-01 14:56:03 -0500

    firebase deployment

--- src/utilities/firebase.ts
+++ src/utilities/firebase.ts
@@ -3,7 +3,13 @@ import { getDatabase, ref, onValue } from 'firebase/database';
 import { useEffect, useState } from 'react';
 
 const firebaseConfig = {
-
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
