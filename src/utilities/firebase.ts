6400bcc2f1eb5c3360925d93919e0df7ee7929e0
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2026-06-01 14:52:18 -0500

    Removed secrets oops

--- src/utilities/firebase.ts
+++ src/utilities/firebase.ts
@@ -3,13 +3,7 @@ import { getDatabase, ref, onValue } from 'firebase/database';
 import { useEffect, useState } from 'react';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5P6wx89BnButu2HWiR_m13lHz5O2U3eE",
-  authDomain: "sample-project-8cb2c.firebaseapp.com",
-  databaseURL: "https://sample-project-8cb2c-default-rtdb.firebaseio.com",
-  projectId: "sample-project-8cb2c",
-  storageBucket: "sample-project-8cb2c.firebasestorage.app",
-  messagingSenderId: "656904410478",
-  appId: "1:656904410478:web:930e4158cd9dba15db9d7f"
+
 };
 
 const firebase = initializeApp(firebaseConfig);
